199eaac9cbbeb18fd794b8c3d6e667e63fcd6579
azkusi <azkusi1@gmail.com>
2023-03-03 04:24:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import useWindowSize from './hooks/useWindow';
+import ItemSelection from './pages/ItemSelection';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import PreferenceSelection from './pages/PreferenceSelection';
+import PersonalisedResults from './pages/PersonalisedResults';
+
 
 function App() {
+
+
+  const width = useWindowSize().width
+  const height = useWindowSize().height
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{"width": width}} className="App">
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/preference-selection/:id" element={<PreferenceSelection/>}/>
+        <Route path="/personalised-results/:id" element={<PersonalisedResults/>}/>
+      </Routes>
+      
     </div>
   );
 }
